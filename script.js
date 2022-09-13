10e293f338233fcef5a5a781c052799a234b28dd
icabulo <107958292+icabulo@users.noreply.github.com>
2022-09-13 09:55:19 -0500

    tutorial's final step +subtle tweaks

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function testFunction() {
-  window.alert("js is working!");
+function toggelNav() {
+  // window.alert("js is working!");
+  const outerGrid = document.querySelector(".outer-grid"); //be mindful we are doing a query selector for a class .outer-grid
+  // console.log(outerGrid.classList);
+  outerGrid.classList.toggle("OG-expanded");
+
+  const nav = document.querySelector("nav"); //be mindful the query selector here is for an element 'nav'
+  nav.classList.toggle("hide-nav");
 }
